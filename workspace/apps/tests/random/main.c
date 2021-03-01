9ee7f81deac0ff0d7ce133d99c377507068e53b8
nikolas karakotas <nikolas.karakotas@noomio.com.au>
2021-03-01 18:52:15 +1100

    added misuse handler

--- workspace/apps/tests/random/main.c
+++ workspace/apps/tests/random/main.c
@@ -4,17 +4,30 @@
 #include "sodium.h"
 #include "randombytes_custom_random.h"
 
+
+void sodium_misuse_handler(void){
+ puts("sodium_misuse!\r\n");
+}
+
 int main(int argc, char * argv[])
 {
 
 	uint8_t buf[255];
 	puts("random\r\n");
 
-	if(sodium_init() == -1) {
-        puts("sodium_init: fail\r\n");
+
+
+	sodium_set_misuse_handler(sodium_misuse_handler);
+	if(sodium_init() != -1) {
+       	randombytes_set_implementation(&randombytes_custom_implementation);
+		randombytes(buf,sizeof(buf));
     }
 
-	randombytes_set_implementation(&randombytes_custom_implementation);
+
+	puts("\r\n");
+	for(int i=0; i < sizeof(buf)-1; i--)
+		printf("%u ",buf[i]);
+	puts("\r\n");
 	
 	for(;;){
 
@@ -22,6 +35,6 @@ int main(int argc, char * argv[])
 
 	}
 
-	return 1;
+
 }
 
