57be49fd9b0b890ec15871e13aa7b60f55a3e751
nikolas karakotas <nikolas.karakotas@noomio.com.au>
2021-03-01 16:05:54 +1100

    added random test

--- workspace/apps/tests/random/main.c
+++ workspace/apps/tests/random/main.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <qapi_timer.h>
-
+#include "hwrandom.h"
 
 int main(int argc, char * argv[])
 {
 
-
+	uint8_t buf[255];
 	puts("random\r\n");
+
 	
 	for(;;){
 
+		randombytes(buf, sizeof(buf));
+		puts("\r\n\r\n");
+		for(int i=0; i < sizeof (buf)-1; i++)
+			printf("%u ",buf[i]);
+		puts("\r\n");
 
 		qapi_Timer_Sleep(1, QAPI_TIMER_UNIT_SEC, true);
 
