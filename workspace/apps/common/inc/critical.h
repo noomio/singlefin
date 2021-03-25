019f17af9b12a6c6b70adc45104476d1d4fdda51
nikolas karakotas <nikolas.karakotas@noomio.com.au>
2021-03-25 16:00:12 +1100

    added prefix

--- workspace/apps/common/inc/critical.h
+++
@@ -1,7 +0,0 @@
-#ifndef __920X_CRITICAL_H__
-#define __920X_CRITICAL_H__
-
-#define TX_ENTER_CRITICAL()                            do { __asm("cpsid i"); } while(0)
-#define TX_EXIT_CRITICAL()                             do { __asm("cpsie i"); } while(0)
-
-#endif
--- workspace/apps/common/inc/hwrandom.h
+++
@@ -1,9 +0,0 @@
-#ifndef __920X_HWRANDOM_H__
-#define __920X_HWRANDOM_H__
-
-
-#include <stdio.h>
-
-void hwrandombytes(uint8_t *buf, uint64_t size );
-
-#endif
